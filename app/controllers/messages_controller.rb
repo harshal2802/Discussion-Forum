abec3bc446ba9b8a7f29cde95ed219ef56f0f7bb
harshal2802 <harshal2802@gmail.com>
2016-04-22 10:30:34 +0000

    add model and controller for message with some methods

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,52 @@
+class MessagesController < ApplicationController
+    before_action :find_message, only: [:show, :edit, :update, :destroy]
+    
+    def index
+        #@messages = Message.where(user_id: current_user)
+        @messages = Message.all.order("created_at DESC")
+    end
+    
+    def new
+        #@message = current_user.docs.build
+        @message = Message.new
+    end
+    
+    def create
+        @message = Message.new(message_params)
+
+		if @message.save
+			redirect_to @message
+		else
+			render 'new'
+		end
+    end
+    
+    def show
+    end
+    
+    def edit
+    end
+    
+    def update
+        if @message.update(message_params)
+			redirect_to @message
+		else
+			render 'edit'
+		end
+    end
+    
+    def destroy
+        @message.destroy
+		redirect_to messages_path
+    end
+    
+    private
+    
+    def find_message
+        @message = Message.find(params[:id])
+    end
+    
+    def message_params
+        params.require(:message).permit(:title, :description)
+    end
+end
